d013b92d808bcc643b30cbb630eb8e6450b760da
Alfredo Ficcadenti <aficcadenti@homeaway.com>
2019-10-25 16:54:02 +0100

    WIP product list

--- src/server/routes/api/productList.js
+++ src/server/routes/api/productList.js
@@ -1,59 +1,65 @@
-const sample = {
-    "links": [
-      {
-        "rel": "self",
-        "href": "https://rambo-annotated.tools.expedia.com/api/products?limit=2"
-      }
-    ],
-    "content": [
-      {
-        "id": 1,
-        "name": "Traffic Engineering",
-        "lineOfBusiness": "Shared Platform and Operations",
-        "lineOfBusinessId": 1,
-        "lineOfBusinessTagFriendlyName": "Shared Platform and Operations",
-        "productTagFriendlyName": "Traffic Engineering",
-        "active": true,
-        "links": [
-          {
-            "rel": "self",
-            "href": "https://rambo-annotated.tools.expedia.com/api/products/1"
-          },
-          {
-            "rel": "lineOfBusiness",
-            "href": "https://rambo-annotated.tools.expedia.com/api/linesOfBusinesses/1"
-          }
-        ]
-      },
-      {
-        "id": 2,
-        "name": "Quality Engineering",
-        "lineOfBusiness": "Shared Platform and Operations",
-        "lineOfBusinessId": 1,
-        "lineOfBusinessTagFriendlyName": "Shared Platform and Operations",
-        "productTagFriendlyName": "Quality Engineering",
-        "active": true,
-        "links": [
-          {
-            "rel": "self",
-            "href": "https://rambo-annotated.tools.expedia.com/api/products/2"
-          },
-          {
-            "rel": "lineOfBusiness",
-            "href": "https://rambo-annotated.tools.expedia.com/api/linesOfBusinesses/1"
-          }
-        ]
-      }
-    ]
-  }
+// const sample = {
+//     "links": [
+//       {
+//         "rel": "self",
+//         "href": "https://rambo-annotated.tools.expedia.com/api/products?limit=2"
+//       }
+//     ],
+//     "content": [
+//       {
+//         "id": 1,
+//         "name": "Traffic Engineering",
+//         "lineOfBusiness": "Shared Platform and Operations",
+//         "lineOfBusinessId": 1,
+//         "lineOfBusinessTagFriendlyName": "Shared Platform and Operations",
+//         "productTagFriendlyName": "Traffic Engineering",
+//         "active": true,
+//         "links": [
+//           {
+//             "rel": "self",
+//             "href": "https://rambo-annotated.tools.expedia.com/api/products/1"
+//           },
+//           {
+//             "rel": "lineOfBusiness",
+//             "href": "https://rambo-annotated.tools.expedia.com/api/linesOfBusinesses/1"
+//           }
+//         ]
+//       },
+//       {
+//         "id": 2,
+//         "name": "Quality Engineering",
+//         "lineOfBusiness": "Shared Platform and Operations",
+//         "lineOfBusinessId": 1,
+//         "lineOfBusinessTagFriendlyName": "Shared Platform and Operations",
+//         "productTagFriendlyName": "Quality Engineering",
+//         "active": true,
+//         "links": [
+//           {
+//             "rel": "self",
+//             "href": "https://rambo-annotated.tools.expedia.com/api/products/2"
+//           },
+//           {
+//             "rel": "lineOfBusiness",
+//             "href": "https://rambo-annotated.tools.expedia.com/api/linesOfBusinesses/1"
+//           }
+//         ]
+//       }
+//     ]
+//   }
 
 module.exports = {
     method: 'GET',
     path: '/api/product-list',
     options: {
         id: 'api-product-list',
-        handler() {
-                return sample.content;
+        handler: (req,h) => {
+          return h.proxy({
+            host: 'opxhub-service.us-west-2.test.expedia.com/api/v1/incidents',
+            port: '8080',
+            protocol: 'http'
+          })
+               
+                // return sample.content;
                 //     .then((res) => res)
                 //     .catch((err) => {
                 //         req.log(['api'],['productList'], err)
--- src/universal/pages/ResiliencyQuestionnaire/index.js
+++ src/universal/pages/ResiliencyQuestionnaire/index.js
@@ -1,31 +1,50 @@
 import React, {Component, Fragment} from 'react';
+import SearchableList from '@homeaway/react-searchable-list';
 import PropTypes from 'prop-types';
+import './styles.less';
 
 class ResiliencyQuestionnaire extends Component {
     constructor(props) {
         super(props);
+
+        const options=['apple',
+        'apricot',
+        'avocado',
+    ];
+
+    const inputProps = {
+        label: 'Product name',
+        id: 'searchable-list-input'
+    };
+
         this.state = {
+            options,
+            inputProps,
+            productList: []
         };
     }
     
     loadProductList = () => {
-        // fetch('api/incident-data')
-        //     .then((resp) => {
-        //         if (!resp.ok) {
-        //             this.setState({error: true});
-        //             throw new Error;
-        //         }
-        //         return resp.json();
-        //     })
-        //     .then((data) => {
-        //         const allIncidents = this.getAllIncidents(data.recordset);
-        //         this.setState({
-        //             allIncidents,
-        //             isLoading: false
-        //         })
-        //     })
-        //     // eslint-disable-next-line no-console
-        //     .catch((error) => {console.log(error)})
+        fetch('api/product-list')
+            .then((resp) => {
+                if (!resp.ok) {
+                    this.setState({error: true});
+                    throw new Error;
+                }
+                return resp.json();
+            })
+            .then((data) => {
+                // eslint-disable-next-line no-console
+                //console.log(Object.keys(data))
+                const productList = Object.values(data).map(prod => prod.name)
+                // eslint-disable-next-line no-console
+                console.log(productList)
+                this.setState({
+                    productList
+                })
+            })
+            // eslint-disable-next-line no-console
+            .catch((error) => {console.log(error)})
     }
     
     componentDidMount() {
@@ -33,13 +52,17 @@ class ResiliencyQuestionnaire extends Component {
     }
     
     render() {
+        const {
+            productList,
+            inputProps
+        } = this.state;
         return (
             <Fragment>
                 <h1>Resiliency Questionnaire</h1>
-                    {/*                 
-                    <LoadingContainer isLoading={isLoading} error={error}>
-                    { allIncidents.length}
-                    </LoadingContainer> */}
+                <SearchableList 
+                    options={productList} 
+                    inputProps={inputProps}
+                />
             </Fragment>
         );
     }
