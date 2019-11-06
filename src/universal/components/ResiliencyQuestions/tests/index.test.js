1320992f3e68461daad675dc081a80eb5ff56f89
Alfredo Ficcadenti <aficcadenti@vrbo.com>
2019-11-06 15:43:36 +0000

    Opex 1404 historical quest (#21)
    added snapshot test

--- src/universal/components/ResiliencyQuestions/tests/index.test.js
+++ src/universal/components/ResiliencyQuestions/tests/index.test.js
@@ -3,15 +3,13 @@ import {mount} from 'enzyme';
 import chai, {expect} from 'chai';
 import chaiJestSnapshot from 'chai-jest-snapshot';
 import ResiliencyQuestion from '../index';
-// import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 chai.use(chaiJestSnapshot);
 
 const questions = [
     {id: 1, question: 'Regions'},
     {id: 2, question: '#AZs'},
 ]
-
-// const snapShot = '<div class="loading-container"><div class="resiliency-question form-group floating-label empty"><textarea class="FormTextArea__textarea form-control FormTextArea__textarea--auto-expand" aria-label="Regions" id="Regions" style="max-height: 15em; overflow: hidden; height: 2px;"></textarea><label class="FormTextArea__floating-label" for="Regions">Regions</label></div><div class="resiliency-question form-group floating-label empty"><textarea class="FormTextArea__textarea form-control FormTextArea__textarea--auto-expand" aria-label="#AZs" id="#AZs" style="max-height: 15em; overflow: hidden; height: 2px;"></textarea><label class="FormTextArea__floating-label" for="#AZs">#AZs</label></div></div>';
 
 describe('<ResiliencyQuestion /> ', () => {
 
@@ -30,14 +28,14 @@ describe('<ResiliencyQuestion /> ', () => {
         expect(wrapper).to.have.lengthOf(1);
     });
 
-    // it('matches the snapshot', () => {
-    //     const props = {
-    //         error: '',
-    //         isLoading: false,
-    //         questions
-    //     };
-    //     chaiJestSnapshot.setTestName("matches the snapshot");
-    //     const wrapper = renderer.create(<ResiliencyQuestion {...props} />);
-    //     expect(wrapper).to.matchSnapshot();
-    //});
+    it('matches the snapshot', () => {
+        const props = {
+            error: '',
+            isLoading: false,
+            questions
+        };
+        chaiJestSnapshot.setTestName("matches the snapshot");
+        const wrapper = renderer.create(<ResiliencyQuestion {...props} />);
+        expect(wrapper).to.matchSnapshot();
+    });
 })
