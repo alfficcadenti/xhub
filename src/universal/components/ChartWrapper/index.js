97d1472f5986d9d0edc5fed0f9651858fbf9dabe
Ivan Shytikov <ishytik@softserveinc.com>
2020-02-05 13:49:10 +0200

    review fixes: removed binded function

--- src/universal/components/ChartWrapper/index.js
+++ src/universal/components/ChartWrapper/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactEcharts from 'echarts-for-react';
-import {financialImpactTooltipFormatter} from '../../components/utils/tooltipFormatter';
 import './styles.less';
 
 const tooltipBgColor = 'gainsboro';
-
+const formatToUSDollarCurrency = (number) => number.
+    toLocaleString('en-US', {style: 'currency', currency: 'USD'});
 const setChartOptions = (series = [], xAxisValues = [], tooltipData, yAxisName) => ({
     legend: {
         data: series.map((item) => item.name)
     },
     tooltip: tooltipData ? {
         trigger: 'item',
-        formatter: financialImpactTooltipFormatter.bind(null, tooltipData),
+        formatter({name, seriesName}) {
+            const incidents = tooltipData[name][seriesName];
+            const incidentsString = incidents.map((item) => {
+                return `<div class="incident-wrapper">
+                        <span class="incident-number">${item.incidentNumberLink}</span>
+                        <span class="incident-financial-impact">${formatToUSDollarCurrency(item.lostRevenue)}</span>
+                        </div>`;
+            }).join('');
+
+            return `<div class="financial-impact-tooltip">
+                <p class="brand-name">${seriesName}</p>
+                ${incidentsString}
+            </div>`;
+        },
         position(point) {
             return [point[0], point[1]];
         },
