26790bcb13ac0b367b862f311a747b86c7678283
Alfredo Ficcadenti <aficcadenti@homeaway.com>
2020-04-16 15:26:59 +0100

    empty array if no content

--- src/universal/pages/ResiliencyQuestionnaire/index.js
+++ src/universal/pages/ResiliencyQuestionnaire/index.js
@@ -93,16 +93,15 @@ class ResiliencyQuestionnaire extends Component {
     loadLoBList = () => {
         fetch('/api/v1/lob')
             .then((resp) => {
-                if (!resp.ok || resp.type !== 'ErrorResponse') {
+                if (!resp.ok) {
                     this.setState({lobError: 'Error: Lines of Business list not available. Try to refresh'});
                     throw new Error();
                 }
                 return resp.json();
             })
             .then((data) => {
-                console.log(data);
                 this.setState({
-                    lobs: data.content
+                    lobs: data.content || []
                 });
             })
             .catch((error) => {
