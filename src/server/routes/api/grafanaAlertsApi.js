8b6eab48d2c373a01f7a54d96f033fa5b8aefea5
Alfredo Ficcadenti <aficcadenti@homeaway.com>
2020-04-20 14:30:33 +0100

    test connectivity from test env

--- src/server/routes/api/grafanaAlertsApi.js
+++ src/server/routes/api/grafanaAlertsApi.js
@@ -11,22 +11,22 @@ module.exports = {
     },
     handler: async (req) => {
         try {
-            console.log('REACHED CODE HERE = ');
             const client = ServiceClient.create('garafana-alerts-api', {
-                //hostname: 'grafana.sea.corp.expecn.com',
+                // hostname: 'grafana.sea.corp.expecn.com',
                 hostname: 'netperf.tools.expedia.com',
                 port: '443',
                 protocol: 'https:',
             });
-            const response = await client.request({
+            const {payload} = await client.request({
                 method: 'GET',
-                //path: '/api/alerts?dashboardId=2074',
+                // path: '/api/alerts?dashboardId=2074',
                 path: '/api/alerts?dashboardId=2399',
                 operation: 'GET_grafana_alerts_data',
                 connectTimeout: 30000
             });
-            console.log('Grafana alerts data fetched successfully...');
-            return response.statusCode;
+            req.log('Grafana alerts data fetched successfully...');
+            req.log(payload);
+            return payload;
         } catch (e) {
             req.log('error occurred while fetching grafana alerts data [ERROR]', e);
             return e;
