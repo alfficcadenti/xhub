f41fcc857d187db6085b1c6ebc86845480fe3916
Alfredo Ficcadenti <aficcadenti@vrbo.com>
2020-04-30 14:23:46 +0100

    Opex 121 (#225)
    * [OPEX-121] Update dependencies
    
    * OktaAuth
    
    * Fix CleanWebpackPlugin v3 using named imports
    
    * Use default cleanwebpackplugin
    
    * Clean up
    
    * Update test snapshots
    
    * removed redux
    
    * build info empty

--- src/server/status.js
+++ src/server/status.js
@@ -1,16 +1,16 @@
 const debug = require('debug')('catalyst-app:build-info');
 const BUILD_INFO_FILE_PATH = '../../config/buildInfo';
 
-const isProd = process.env.NODE_ENV === 'production';
-const cdnUrl = process.env.CDN_URL;
+// const isProd = process.env.NODE_ENV === 'production';
+// const cdnUrl = process.env.CDN_URL;
 
 // used for `/buildInfo` endpoint.
 
 let buildInfo;
 try {
-    if (isProd && cdnUrl) {
-        buildInfo = require(BUILD_INFO_FILE_PATH);
-    }
+    // if (isProd && cdnUrl) {
+    buildInfo = require(BUILD_INFO_FILE_PATH);
+    // }
 } catch (err) {
     debug('Failed to load build config: %O', err);
     buildInfo = {version: 'unknown'};
