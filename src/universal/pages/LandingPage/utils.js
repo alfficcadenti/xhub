19155b569741396296ecf1a9046096b8a9711775
Ivan Shytikov <ishytik@softserveinc.com>
2020-07-28 12:24:10 +0300

    change CSR response props

--- src/universal/pages/LandingPage/utils.js
+++ src/universal/pages/LandingPage/utils.js
@@ -15,7 +15,7 @@ export const formatCSRData = (data, selectedBrands) => {
     const CSRDataFormatted = selectedBrands.length && Array.isArray(selectedBrands) ? selectedBrands.map((brand) => {
         const csrData = data && data.map(
             (x) => {
-                return x.checkoutSuccessPercentagesData && x.checkoutSuccessPercentagesData.find((item) => mapBrandNames(item.brand) === brand) ? x.checkoutSuccessPercentagesData.find((item) => mapBrandNames(item.brand) === brand).rate : '';
+                return x.successRatePercentagesData && x.successRatePercentagesData.find((item) => mapBrandNames(item.brand) === brand) ? x.successRatePercentagesData.find((item) => mapBrandNames(item.brand) === brand).rate : '';
             }).filter((n) => n && !isNaN(n));
         return {brandName: brand, CSRTrend: csrData};
     }) :
