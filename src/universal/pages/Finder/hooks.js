c4cc65911a99540a76356b620f1091ae2023be62
Ivan Shytikov <ishytik@softserveinc.com>
2020-09-08 21:12:48 +0300

    integrate Universal Search component into the page views page annotations

--- src/universal/pages/Finder/hooks.js
+++ src/universal/pages/Finder/hooks.js
@@ -35,7 +35,7 @@ export const useFetchCRs = (
             fetch(url)
                 .then(checkResponse)
                 .then((data) => {
-                    const filteredCR = brand && brand.changeRequests ? data.filter(x=>x.platform === brand.changeRequests) : data;
+                    const filteredCR = brand && brand.changeRequests ? data.filter((x) => x.platform === brand.changeRequests) : data;
                     const crs = sortArrayByMostRecentDate(filteredCR, 'openedAt');
                     const uniqueCRs = getUniqueByProperty(crs, 'number');
                     const adjustedUniqueCRs = adjustCRsProperties(uniqueCRs);
@@ -48,13 +48,13 @@ export const useFetchCRs = (
                     const dataStatuses = getListOfUniqueProperties(adjustedUniqueCRs, 'status').sort();
 
                     setIndexedDataForSuggestions({
-                        'productName': dataProducts,
-                        'applicationName': dataApplications,
-                        'platform': dataPlatforms,
-                        'businessReason': dataBusinessReasons,
-                        'number': dataCRnumbers,
-                        'status': dataStatuses,
-                        'team': dataTeams,
+                        productName: dataProducts,
+                        applicationName: dataApplications,
+                        platform: dataPlatforms,
+                        businessReason: dataBusinessReasons,
+                        number: dataCRnumbers,
+                        status: dataStatuses,
+                        team: dataTeams,
                     });
                     setAllUniqueCRs(adjustedUniqueCRs);
                     setAllCRs(crs);
