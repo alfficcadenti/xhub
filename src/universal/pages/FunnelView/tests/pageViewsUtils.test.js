1ab8726b1bfd8c9efc43f44545fc23ab11aac19a
Alfredo Ficcadenti <aficcadenti@vrbo.com>
2020-10-15 11:19:01 +0100

    [NO-JIRA] test for page views (#508)
    * consolidated make object for chart functions in pageutils
    
    * WIP
    
    * consolidated pattern for page views and added tests for makePageViewObjects
    
    * removed console.log
    
    * label for tooltip changed
    
    * updated test
    
    * mistake on endpoint change
    
    Co-authored-by: Alfredo Ficcadenti <aficcadenti@homeaway.com>

--- src/universal/pages/FunnelView/tests/pageViewsUtils.test.js
+++ src/universal/pages/FunnelView/tests/pageViewsUtils.test.js
@@ -1,10 +1,10 @@
 import {expect} from 'chai';
-import {makePageViewLoBObjects} from '../pageViewsUtils';
+import {makePageViewLoBObjects, makePageViewObjects} from '../pageViewsUtils';
 import {EXPEDIA_BRAND, PAGES_LIST} from '../../../constants';
 import moment from 'moment';
 import 'moment-timezone';
-import {pageViewLoBEndpoint} from '../mockData';
-import {emptyMockResults, mockResults} from './mockResults';
+import {pageViewLoBEndpoint, pageViewMockData} from '../mockData';
+import {emptyMockResults, mockResults, pageViewsMockResults, emptyPageViewsMockResults} from './mockResults';
 
 describe('makePageViewLoBObjects component testing', () => {
     const start = moment('2020-08-10');
@@ -24,5 +24,26 @@ describe('makePageViewLoBObjects component testing', () => {
 
     it('creates an array with each object and the expected format', () => {
         expect(makePageViewLoBObjects(pageViewLoBEndpoint, start, end, EXPEDIA_BRAND)).to.eql(mockResults);
+    });
+});
+
+describe('makePageViewObjects component testing', () => {
+    const start = moment('2020-08-10');
+    const end = moment('2020-08-11');
+    it('creates an array with one element for each page view in PAGES_LIST', () => {
+        expect(makePageViewObjects([], start, end, EXPEDIA_BRAND)).to.have.length(PAGES_LIST.length);
+    });
+
+    it('returns array with objects if no inputs are passed', () => {
+        expect(makePageViewObjects()).to.eql(emptyPageViewsMockResults);
+    });
+
+    test('each array element has lob: undefined', () => {
+        const lobArray = makePageViewObjects([], start, end, EXPEDIA_BRAND);
+        lobArray.forEach((x) => expect(x.lob).to.be.undefined);
+    });
+
+    it('creates an array with each object and the expected format', () => {
+        expect(makePageViewObjects(pageViewMockData, start, end, EXPEDIA_BRAND)).to.eql(pageViewsMockResults);
     });
 });
