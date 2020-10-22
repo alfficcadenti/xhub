fa5cce21b4dfa5fcbe782aaa080843d9d9628004
Alfredo Ficcadenti <aficcadenti@vrbo.com>
2020-10-22 11:19:32 +0100

    NO-JIRA check error data type in Loading Component (#519)
    Co-authored-by: Alfredo Ficcadenti <aficcadenti@homeaway.com>

--- src/universal/components/LoadingContainer/tests/LoadingContainer.test.js
+++ src/universal/components/LoadingContainer/tests/LoadingContainer.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {expect} from 'chai';
-import {shallow} from 'enzyme';
+import {shallow, render} from 'enzyme';
 import LoadingContainer from '../';
 
 
@@ -12,9 +12,25 @@ describe('<LoadingContainer />', () => {
         expect(wrapper).to.have.length(1);
     });
 
-    it('checks LineChartWrapper component exists', () => {
+    it('checks LoadingContainer component exists', () => {
         wrapper = shallow(<LoadingContainer isLoading={false} />);
         expect(wrapper).to.have.length(1);
         expect(wrapper.find('.LoadingOverlay__li').children()).to.have.length(0);
+    });
+
+    it('renders correctly and display alert message', () => {
+        wrapper = render(<LoadingContainer isLoading error={'test'}/>);
+        console.log(wrapper.text());
+        expect(wrapper.text()).to.be.eql('test');
+    });
+
+    it('renders correctly and display the standard alert message: Error', () => {
+        wrapper = render(<LoadingContainer isLoading error={{ErrorMessage: 'test'}}/>);
+        expect(wrapper.text()).to.be.eql('Error');
+    });
+
+    it('renders correctly and display the standard alert message: Error', () => {
+        wrapper = render(<LoadingContainer isLoading error={[1, 2, 3]}/>);
+        expect(wrapper.text()).to.be.eql('Error');
     });
 });
