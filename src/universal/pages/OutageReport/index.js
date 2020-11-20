d69b6dd5b79f7d5c53eb6f8c5dbc9e5f782787e9
Ivan Shytikov <ishytik@softserveinc.com>
2020-11-20 15:49:10 +0200

    add outage report to the header and some changes to the  component

--- src/universal/pages/OutageReport/index.js
+++ src/universal/pages/OutageReport/index.js
@@ -1,35 +1,15 @@
-import React, {useState} from 'react';
-import {RadioButton, RadioGroup} from '@homeaway/react-form-components';
+import React from 'react';
 import Iframe from 'react-iframe';
 import './styles.less';
 
-const weeklyReportUrl = 'WeeklyOutageScorecard_15843878097700';
-const monthlyReportUrl = 'MonthlyOutageScorecard';
-
 
 const OutageReport = () => {
-    const [isWeekly, setIsWeekly] = useState(true);
-
     return (
         <div className="outage-report-container">
             <h1 className="page-title">{'Outage report'}</h1>
-            <RadioGroup name="choice" ariaLabel="Outage report choices">
-                <RadioButton
-                    label="Weekly outage report"
-                    value="monthly"
-                    checked={isWeekly}
-                    onChange={() => setIsWeekly(true)}
-                />
-                <RadioButton
-                    label="Monthly outage report"
-                    value="monthly"
-                    checked={!isWeekly}
-                    onChange={() => setIsWeekly(false)}
-                />
-            </RadioGroup>
             <div className="outage-report">
                 <Iframe
-                    url={`https://tableau.sea.corp.expecn.com/views/${isWeekly ? weeklyReportUrl : monthlyReportUrl}/Scorecard?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Ftableau.sea.corp.expecn.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=yes&:showAppBanner=false&:display_spinner=no&:loadOrderID=0`}
+                    url="https://tableau.sea.corp.expecn.com/views/OutageScorecard/OutageScorecard?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Ftableau.sea.corp.expecn.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=yes&:showAppBanner=false&:display_spinner=no&:loadOrderID=0"
                     key={'iframe'}
                     width="1600px"
                     height="950px"
