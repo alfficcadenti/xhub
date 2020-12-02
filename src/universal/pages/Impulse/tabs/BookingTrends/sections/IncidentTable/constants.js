a0c19b3a598e90c28faa3c5b636ccc96649edb66
polnu <polnu@expediagroup.com>
2020-12-02 13:17:34 +0530

    added missing constants file

---
+++ src/universal/pages/Impulse/tabs/BookingTrends/sections/IncidentTable/constants.js
@@ -0,0 +1,29 @@
+export const INCIDENT_TABLE_COLUMN_HEADERS = ['Summary',
+    'Root Cause',
+    'Brand',
+    'Start Date',
+    'End Date',
+    'Status',
+    'Priority',
+    'Booking Impact'];
+
+export const INCIDENT_TABLE_COLUMNS = ['summary',
+    'rootCause',
+    'brand',
+    'startDate',
+    'endDate',
+    'status',
+    'priority',
+    'bookingImpact'];
+
+export const REVLOSS_TABLE_COLUMN_HEADERS = ['Brand',
+    'LOB',
+    'Rev Loss',
+    'GBV Loss',
+    'Order Loss'];
+
+export const REVLOSS_TABLE_COLUMNS = ['brand',
+    'lob',
+    'revloss',
+    'gbvloss',
+    'orderloss'];
