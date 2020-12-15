1128b6e5553e47a4bf41a406c3b0cdeaca70f655
Ivan Shytikov <ishytik@softserveinc.com>
2020-12-15 17:26:26 +0200

    integrate A/B Tests data on change finder page

--- src/universal/pages/Finder/index.js
+++ src/universal/pages/Finder/index.js
@@ -1,20 +1,35 @@
 /* eslint-disable no-use-before-define */
 import React, {useState, useEffect} from 'react';
 import moment from 'moment';
+import {Navigation} from '@homeaway/react-navigation';
 import LoadingContainer from '../../components/LoadingContainer';
 import DatePicker from '../../components/DatePicker/index';
 import UniversalSearch from '../../components/UniversalSearch';
-import {DATE_FORMAT} from '../../constants';
 
-import {ChangeRequests} from './tabs/index';
+import {DATE_FORMAT} from '../../constants';
+import {ChangeRequests, ABTests} from './tabs/index';
 import {useFetchCRs} from './hooks';
 import {useSelectedBrand, useQueryParamChange, useFetchProductMapping} from '../hooks';
 import './styles.less';
-import {adjustInputValue} from '../utils';
+import {adjustInputValue, checkResponse} from '../utils';
 
 const startDateDefaultValue = moment().subtract(1, 'days').format(DATE_FORMAT);
 const endDateDefaultValue = moment().format(DATE_FORMAT);
 const minDate = moment('2019-01-01').toDate();
+
+
+const navLinks = [
+    {
+        id: 'сhangeRequests',
+        label: 'Change Requests',
+        href: '/finder'
+    },
+    {
+        id: 'abTests',
+        label: 'A/B Tests',
+        href: '/finder'
+    }
+];
 
 const Finder = (props) => {
     const selectedBrand = props.selectedBrands[0];
@@ -24,6 +39,8 @@ const Finder = (props) => {
     const [filteredUniqueCRs, setFilteredUniqueCRs] = useState([]);
     const [isApplyClicked, setIsApplyClicked] = useState(false);
     const [advancedFilter, setAdvancedFilter] = useState([]);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [abTestsAnnotations, setAbTestsAnnotations] = useState([]);
 
     const [
         isLoading,
@@ -43,9 +60,35 @@ const Finder = (props) => {
     useQueryParamChange(selectedBrand, props.onBrandChange);
     useSelectedBrand(selectedBrand, props.onBrandChange, props.prevSelectedBrand);
 
+    useEffect(() => {
+        const fetchAbTestsAnnotations = () => {
+            setAbTestsAnnotations([]);
+            const dateQuery = startDate && endDate
+                ? `?startDate=${moment(startDate).utc().format()}&endDate=${moment(endDate).utc().format()}`
+                : '';
+
+            fetch(`/abTests${dateQuery}`)
+                .then(checkResponse)
+                .then((data) => {
+                    setAbTestsAnnotations(data);
+                })
+                .catch((err) => {
+                    // eslint-disable-next-line no-console
+                    console.error(err);
+                });
+        };
+
+        fetchAbTestsAnnotations();
+    }, [startDate, endDate]);
+
+
     const applyAdvancedFilter = () => {
         const filterNewFormat = adjustInputValue(advancedFilter);
-        let newFilteredCRs = [...allUniqueCRs];
+
+        const currentItems = (activeIndex === 0) ? allUniqueCRs : abTestsAnnotations;
+
+        let newFilteredCRs = [...currentItems];
+
         if (filterNewFormat && filterNewFormat.length) {
             filterNewFormat.forEach((filter) => {
                 if (Array.isArray(filter.values) && filter.values[0]) {
@@ -54,9 +97,10 @@ const Finder = (props) => {
                     newFilteredCRs = newFilteredCRs.filter((x) => x[filter.key] === filter.values);
                 }
             });
+
             setFilteredUniqueCRs(newFilteredCRs);
         } else {
-            setFilteredUniqueCRs([...allUniqueCRs]);
+            setFilteredUniqueCRs([...currentItems]);
         }
     };
 
@@ -80,6 +124,22 @@ const Finder = (props) => {
         setEndDate('');
         setFilteredUniqueCRs([]);
         setIsDirtyForm(true);
+    };
+
+    const handleNavigationClick = (e, activeLinkIndex) => {
+        setActiveIndex(activeLinkIndex);
+    };
+
+    // eslint-disable-next-line complexity
+    const renderTabs = () => {
+        switch (activeIndex) {
+            case 0:
+                return <ChangeRequests filteredCR={filteredUniqueCRs} selectedBrand={selectedBrand} />;
+            case 1:
+                return <ABTests filteredCR={abTestsAnnotations} />;
+            default:
+                return <ChangeRequests filteredCR={filteredUniqueCRs} selectedBrand={selectedBrand} />;
+        }
     };
 
     return (
@@ -113,8 +173,15 @@ const Finder = (props) => {
                 )}
 
             </div>
+            <Navigation
+                noMobileSelect
+                activeIndex={activeIndex}
+                links={navLinks}
+                onLinkClick={handleNavigationClick}
+            />
             <LoadingContainer isLoading={isLoading} error={error}>
-                <ChangeRequests filteredCR={filteredUniqueCRs} selectedBrand={selectedBrand}/>
+                {/* <ChangeRequests filteredCR={filteredUniqueCRs} selectedBrand={selectedBrand}/>*/}
+                {renderTabs()}
             </LoadingContainer>
         </div>
     );
