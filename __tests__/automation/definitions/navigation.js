8107bd6ed54ef30160eb7d5d82560c72ca7db524
pkrajewski <v-pkrajewski@expediagroup.com>
2021-03-19 01:26:44 +0100

    fix: wait for loader not visible

--- __tests__/automation/definitions/navigation.js
+++ __tests__/automation/definitions/navigation.js
@@ -26,7 +26,7 @@ Then(/^user go ahead and clicks on the (.*)/, async element => {
 });
 
 Then(/^user wait for the data to load/, async () => {
-    await waitForElement(pageElements, '@successRatesWidget', 'visible')
+    await waitForElement(pageElements, '@loader', 'not visible')
 });
 
 Then(/^the current page title contain (.*)/, async title => {
--- __tests__/automation/page_objects/common_pageObj.js
+++ __tests__/automation/page_objects/common_pageObj.js
@@ -3,8 +3,8 @@ module.exports = {
         pageTitle: {
             selector: '.page-title'
         },
-        successRatesWidget: {
-            selector: '.success-rates-widget-container'
+        loader: {
+            selector: '.LoadingOverlay'
         }
     }
 };
