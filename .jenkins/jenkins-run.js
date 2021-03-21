d47512f93fadffb9c24c0bba9d5827e07b3772f7
Phil Chen <phchen@expedia.com>
2021-03-20 22:50:54 -0700

    fix(misc): add version
    add version fix

--- .jenkins/jenkins-run.js
+++ .jenkins/jenkins-run.js
@@ -16,53 +16,53 @@ console.info(`Script Type: ${script}`);
 console.info(`DA Report: ${daReports}`);
 
 runAll('clean')
-  .then(() => {
-    console.info('cleaned directory');
-    return runAll(script, parallelConfig);
-  })
-  .catch(err => {
-    console.error(`test job failed with ${err.message}`);
-  })
-  .then(() => {
-    console.info('Creating html report');
-    return runAll(reportScript, parallelConfig);
-  })
-  .catch(error => {
-    console.error('Error creating the html report from test results');
-  })
-  .then(processResults)
-  .catch(err => {
-    console.error(`test result process failed with ${err.message}`);
-  })
-  .then(() => {
-    let deployedVersion;
-    let browserVersion;
-    const versionFile = './reports/buildVersion.txt';
-    const browserVersionFile = './reports/browserVersion.txt';
-    const fs = require('fs');
+    .then(() => {
+      console.info('cleaned directory');
+      return runAll(script, parallelConfig);
+    })
+    .catch(err => {
+      console.error(`test job failed with ${err.message}`);
+    })
+    .then(() => {
+      console.info('Creating html report');
+      return runAll(reportScript, parallelConfig);
+    })
+    .catch(error => {
+      console.error('Error creating the html report from test results');
+    })
+    .then(processResults)
+    .catch(err => {
+      console.error(`test result process failed with ${err.message}`);
+    })
+    .then(() => {
+      let deployedVersion;
+      let browserVersion;
+      const versionFile = './reports/version.txt';
+      const browserVersionFile = './reports/browserVersion.txt';
+      const fs = require('fs');
 
-    console.info('Start to store results into MongoDB');
-    if (fs.existsSync(versionFile)) {
-      deployedVersion = fs.readFileSync(versionFile, 'utf8');
-    } else {
-      deployedVersion = 'UNKNOWN';
-    }
-    if (fs.existsSync(browserVersionFile)) {
-      browserVersion = fs.readFileSync(browserVersionFile, 'utf8');
-    } else {
-      browserVersion = 'UNKNOWN';
-    }
-    if (process.env.shelvedCL !== null && process.env.devId != null) {
-      deployedVersion = `shelved_${process.env.shelvedCL}_${process.env.devId}`;
-    }
-    process.env.WAR_FILE_VERSION = deployedVersion;
-    process.env.BROWSER_VERSION = browserVersion;
-    console.info(
-      `'store results into MongoDB with build version: ${deployedVersion} and browser version: ${browserVersion}`
-    );
-    runAll([`${daReports}`], parallelConfig);
-    console.info(`[info] Project name of ${daReports} was recorded into the DA Database`);
-  })
-  .catch(err => {
-    console.error(`Report Generation Failed: ${err.message}`);
-  });
+      console.info('Start to store results into MongoDB');
+      if (fs.existsSync(versionFile)) {
+        deployedVersion = fs.readFileSync(versionFile, 'utf8');
+      } else {
+        deployedVersion = 'UNKNOWN';
+      }
+      if (fs.existsSync(browserVersionFile)) {
+        browserVersion = fs.readFileSync(browserVersionFile, 'utf8');
+      } else {
+        browserVersion = 'UNKNOWN';
+      }
+      if (process.env.shelvedCL !== null && process.env.devId != null) {
+        deployedVersion = `shelved_${process.env.shelvedCL}_${process.env.devId}`;
+      }
+      process.env.WAR_FILE_VERSION = deployedVersion;
+      process.env.BROWSER_VERSION = browserVersion;
+      console.info(
+          `'store results into MongoDB with build version: ${deployedVersion} and browser version: ${browserVersion}`
+      );
+      runAll([`${daReports}`], parallelConfig);
+      console.info(`[info] Project name of ${daReports} was recorded into the DA Database`);
+    })
+    .catch(err => {
+      console.error(`Report Generation Failed: ${err.message}`);
+    });
--- __tests__/automation/utils/logDeploymentVersion.js
+++ __tests__/automation/utils/logDeploymentVersion.js
@@ -1,45 +1,45 @@
 let isVersionValid = false;
 const path = require('path');
 
-const logVersion = (version) => {
-    const fs = require('fs');
-    const dir = 'reports';
+const logVersion = version => {
+  const fs = require('fs');
+  const dir = 'reports';
 
-    if (!version || version.length === 0) {
-        console.info('[info] Build Info was not found');
-
-        return;
-    }
-
-    const versionFile = path.resolve(dir, 'version.txt');
-    const deployedVersion = version.substring(0, 8);
-
-    console.info(`[info] Version: ${deployedVersion}`);
-
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
-
-    fs.writeFile(versionFile, deployedVersion, (err) => {
-        if (err) {
-            console.error(err);
-
-            return;
-        }
-        console.info(`[info] ${deployedVersion} is created on ${versionFile}`);
-    });
+  if (!version || version.length === 0) {
+    console.info('[info] Build Info was not found');
 
     return;
+  }
+
+  const versionFile = path.resolve(dir, 'version.txt');
+  const deployedVersion = version.substring(0, 8);
+
+  console.info(`[info] Version: ${deployedVersion}`);
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  fs.writeFile(versionFile, deployedVersion, err => {
+    if (err) {
+      console.error(err);
+
+      return;
+    }
+    console.info(`[info] ${deployedVersion} is created on ${versionFile}`);
+  });
+
+  return;
 };
 
-process.argv.forEach((val) => {
-    if (val.includes('version:')) {
-        const argument = val.split(':');
-        logVersion(argument[1]);
-        isVersionValid = true;
-    }
+process.argv.forEach(val => {
+  if (val.includes('version:')) {
+    const argument = val.split(':');
+    logVersion(argument[1]);
+    isVersionValid = true;
+  }
 });
 
 if (!isVersionValid) {
-    console.info('[info] Build Info was not found');
+  console.info('[info] Build Info was not found');
 }
