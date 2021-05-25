91831fb0a5decd11ab5847ab5b3b4622f0e9baa1
Patryk Krajewski <v-pkrajewski@expediagroup.com>
2021-05-25 15:40:53 +0200

    LagIndicator component refactor

--- src/universal/components/LagIndicator/index.js
+++ src/universal/components/LagIndicator/index.js
@@ -7,23 +7,28 @@ import {
     VRBO_BRAND,
 } from '../../constants';
 
-const LagComponent = ({selectedBrand}) => {
-    const hcomIframe = <iframe src="https://app.datadoghq.com/graph/embed?token=8883f4260fb4215683ac747fc140759431ec372c9a22c39d55af9aab56ac306c&height=100&width=200&legend=false" width="200" height="100" frameBorder="0" title="Onestream Lag"/>;
-    const uisprimeIframe = <iframe src="https://app.datadoghq.com/graph/embed?token=1865fed42a02636eb6baa6448ad4671b76604611347794e00d815163a79a18f0&height=100&width=200&legend=false" width="200" height="100" frameBorder="0" title="Uisprime Lag" />;
-    const vrboIframe = <iframe src="https://app.datadoghq.com/graph/embed?token=e59ee51979dd7e1f62453c06cf9f1b34e707cffd6e6ce1d053b6d99ae90896ed&height=100&width=200&legend=true" width="200" height="100" frameBorder="0" title="Vrbo Lag" />;
-
+const selectAttributes = (selectedBrand) => {
     switch (selectedBrand) {
         case HOTELS_COM_BRAND:
-            return hcomIframe;
+            return {token: '8883f4260fb4215683ac747fc140759431ec372c9a22c39d55af9aab56ac306c', title: 'Onestream Lag'};
         case EXPEDIA_PARTNER_SERVICES_BRAND:
-            return uisprimeIframe;
+            return {token: '1865fed42a02636eb6baa6448ad4671b76604611347794e00d815163a79a18f0', title: 'UISPrime Lag'};
         case EXPEDIA_BRAND:
-            return uisprimeIframe;
+            return {token: '1865fed42a02636eb6baa6448ad4671b76604611347794e00d815163a79a18f0', title: 'UISPrime Lag'};
         case VRBO_BRAND:
-            return vrboIframe;
+            return {token: 'e59ee51979dd7e1f62453c06cf9f1b34e707cffd6e6ce1d053b6d99ae90896ed', title: 'Vrbo Lag'};
         default:
-            return null;
+            return {token: null, title: null};
     }
+};
+
+const LagComponent = ({selectedBrand}) => {
+    const {token, title} = selectAttributes(selectedBrand);
+
+    if (token && title) {
+        return <iframe src={`https://app.datadoghq.com/graph/embed?token=${token}&height=100&width=200&legend=false"`} width="200" height="100" frameBorder="0" title={title}/>;
+    }
+    return null;
 };
 
 export default React.memo(LagComponent);
--- src/universal/components/LagIndicator/tests/index.test.js
+++ src/universal/components/LagIndicator/tests/index.test.js
@@ -3,17 +3,16 @@ import {shallow} from 'enzyme';
 import LagIndicator from '../index';
 import {
     EXPEDIA_BRAND,
-    EGENCIA_BRAND,
 } from '../../../constants';
 
 describe('<LagIndicator /> ', () => {
     it('renders correctly', () => {
         const wrapper = shallow(<LagIndicator selectedBrand={EXPEDIA_BRAND} />);
-        expect(wrapper).toHaveLength(1);
+        expect(wrapper.find('iframe')).toHaveLength(1);
     });
 
     it('does not render when invalid brand', () => {
-        const wrapper = shallow(<LagIndicator selectedBrand={EGENCIA_BRAND} />);
-        expect(wrapper).toHaveLength(1);
+        const wrapper = shallow(<LagIndicator selectedBrand={''} />);
+        expect(wrapper.find('iframe')).toHaveLength(0);
     });
 });
