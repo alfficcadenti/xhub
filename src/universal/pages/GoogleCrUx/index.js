32eebf5ee462db261872693c6f526170929ad61d
Patryk Krajewski <v-pkrajewski@expediagroup.com>
2021-07-05 11:45:39 +0200

    Add brand filter to Google Crux & Partner Metrics

--- src/universal/pages/GoogleCrUx/index.js
+++ src/universal/pages/GoogleCrUx/index.js
@@ -3,9 +3,11 @@ import {withRouter} from 'react-router-dom';
 import './styles.less';
 import GrafanaDashboard from '../../components/GrafanaDashboard';
 
-const GoogleCrUx = () => {
+const GoogleCrUx = ({selectedBrands, availableBrands}) => {
     return (
         <GrafanaDashboard
+            selectedBrands={selectedBrands}
+            availableBrands={availableBrands}
             name="google-crux"
             title="Google CrUx (RUM)"
             url="https://grafana.test.expedia.com/d/d0wMYdVZV/egperf_crux?theme=light&kiosk=tv&var-site=vrbo.com&var-scenario=Origin&var-formFactor=PHONE"
--- src/universal/pages/PartnerBusinessMetrics/index.js
+++ src/universal/pages/PartnerBusinessMetrics/index.js
@@ -3,9 +3,11 @@ import {withRouter} from 'react-router-dom';
 import './styles.less';
 import GrafanaDashboard from '../../components/GrafanaDashboard';
 
-const PartnerBusinessMetrics = () => {
+const PartnerBusinessMetrics = ({selectedBrands, availableBrands}) => {
     return (
         <GrafanaDashboard
+            selectedBrands={selectedBrands}
+            availableBrands={availableBrands}
             name="partner-business-metrics"
             title="Partner Business Metrics"
             url="https://opexhub-grafana.expedia.biz/d/2D1iGCgnk/vrbo-business-partner-metrics?orgId=1&theme=light"
