9321f571bed60dbbcd7daf582e026a02d08f3747
James Kim <jamkim@vrbo.com>
2021-09-17 14:05:05 -0500

    [CRET-2350] Fix thresholds for portfolio ttx metrics (#882)
    Co-authored-by: jamkim <jamkim@homeaway.com>

--- src/universal/pages/PortfolioScoreCard/ScoreCard/constants.js
+++ src/universal/pages/PortfolioScoreCard/ScoreCard/constants.js
@@ -5,21 +5,21 @@ export const ORGS = ['l1', 'l2', 'l3', 'l4'];
 
 export const L1_ORGS_LABEL = 'L1 Orgs';
 
-export const SCORECARD_COLUMNS = ['Organization', 'P1', 'P2', 'TTD<=15m', 'TTF<=15m', 'TTK<=30m', 'TTR<=60m', 'Corrective Actions'];
+export const SCORECARD_COLUMNS = ['Organization', 'P1', 'P2', 'TTD<=15m', 'TTK<=30m', 'TTF<=15m', 'TTR<=60m', 'Corrective Actions'];
 
 export const SCORECARD_COLUMNS_INFO = {
     'TTD<=15m': <div>{'Time to Detect with >=75% highlighted'}</div>,
-    'TTF<=15m': <div>{'Time to Fix with >=75% highlighted'}</div>,
     'TTK<=30m': <div>{'Time to Know with >=75% highlighted'}</div>,
-    'TTR<=60m': <div>{'Time to Resolve with >=75% highlighted'}</div>
+    'TTF<=15m': <div>{'Time to Fix with >=75% highlighted'}</div>,
+    'TTR<=60m': <div>{'Time to Restore with >=75% highlighted'}</div>
 };
 
 export const setPercentThresholdClass = (val) => Number(`${val}`.match(/\d+/)[0]) >= 75 ? 'success-cell' : '';
 
 export const SCORECARD_RULES = [
     {column: 'TTD<=15m', setClass: setPercentThresholdClass},
-    {column: 'TTF<=15m', setClass: setPercentThresholdClass},
     {column: 'TTK<=30m', setClass: setPercentThresholdClass},
+    {column: 'TTF<=15m', setClass: setPercentThresholdClass},
     {column: 'TTR<=60m', setClass: setPercentThresholdClass}
 ];
 
@@ -35,16 +35,16 @@ export const TICKET_DETAILS_COLUMNS = [
 
 export const TICKET_DETAILS_INFO = {
     'Time To Detect': <div>{'Time to Detect in minutes'}<br />{'with <=15m highlighted'}</div>,
-    'Time To Fix': <div>{'Time to Fix in minutes'}<br />{'with <=15m highlighted'}</div>,
     'Time To Know': <div>{'Time to Know in minutes'}<br />{'with <=30m highlighted'}</div>,
-    'Time To Restore': <div>{'Time to Resolve in minutes'}<br />{'with <=60m highlighted'}</div>
+    'Time To Fix': <div>{'Time to Fix in minutes'}<br />{'with <=15m highlighted'}</div>,
+    'Time To Restore': <div>{'Time to Restore in minutes'}<br />{'with <=60m highlighted'}</div>
 };
 
 export const generateSetThresholdClass = (threshold) => (val) => Number(`${val}`.match(/\d+/)[0]) <= threshold ? 'success-cell' : '';
 
 export const TICKET_DETAILS_RULES = [
     {column: 'Time To Detect', setClass: generateSetThresholdClass(15)},
-    {column: 'Time To Fix', setClass: generateSetThresholdClass(15)},
     {column: 'Time To Know', setClass: generateSetThresholdClass(30)},
+    {column: 'Time To Fix', setClass: generateSetThresholdClass(15)},
     {column: 'Time To Restore', setClass: generateSetThresholdClass(60)},
 ];
