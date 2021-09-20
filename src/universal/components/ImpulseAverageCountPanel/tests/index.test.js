31c8d94cf7984a6a0cec273bb972d33b7525fb8b
Archana Ahirwar <aahirwar@expediagroup.com>
2021-09-20 12:03:00 +0530

    modified test cases

--- src/universal/components/ImpulseAverageCountPanel/tests/index.test.js
+++ src/universal/components/ImpulseAverageCountPanel/tests/index.test.js
@@ -4,16 +4,17 @@ import {shallow} from 'enzyme';
 import ImpulseAverageCountPanel from '../index';
 
 const percentageChangeData = {
-    weekly: [{overall: 0}, {brands: {Vrbo: -10.39, Travelocity: -19.39, Egencia: 178.88, 'CarRentals.com': 5.6}}, {lobs: {Activities: -20.69, Rail: 72.86, Cruise: -23.81, Car: -2.98}}],
-    monthly: [{overall: 0}, {brands: {Vrbo: -10.39, Travelocity: -19.39, Egencia: 178.88, 'CarRentals.com': 5.6}}, {lobs: {Activities: -20.69, Rail: 72.86, Cruise: -23.81, Car: -2.98}}],
-    yearly: [{overall: 0}, {brands: {Vrbo: -10.39, Travelocity: -19.39, Egencia: 178.88, 'CarRentals.com': 5.6}}, {lobs: {Activities: -20.69, Rail: 72.86, Cruise: -23.81, Car: -2.98}}],
+    selectedLobs: '',
+    weekly: -3.88,
+    monthly: 5.65,
+    yearly: 'N/A'
 };
 
 describe('ImpulseAverageCountPanel component testing', () => {
     let wrapper;
 
     beforeEach(() => {
-        wrapper = shallow(<ImpulseAverageCountPanel data={percentageChangeData} activeIndex={1} isLoading={false} />);
+        wrapper = shallow(<ImpulseAverageCountPanel data={percentageChangeData} isLoading={false} />);
     });
 
     afterEach(() => {
@@ -26,6 +27,6 @@ describe('ImpulseAverageCountPanel component testing', () => {
 
     it('renders correct container when there is no errors', () => {
         wrapper.setProps({isLoading: false});
-        expect(wrapper.render().find('.percentage-change-card-container')).to.have.length(1);
+        expect(wrapper.render().find('.percentage-change-container')).to.have.length(1);
     });
 });
