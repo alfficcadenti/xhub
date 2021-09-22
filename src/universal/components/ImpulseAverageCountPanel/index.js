e0ec950b7625eef3911957ad604b0da4df7eeae8
Archana Ahirwar <aahirwar@expediagroup.com>
2021-09-22 10:08:26 +0530

    resolve comments

--- src/universal/components/ImpulseAverageCountPanel/index.js
+++ src/universal/components/ImpulseAverageCountPanel/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import LoadingContainer from '../LoadingContainer';
 import PropTypes from 'prop-types';
+import {FETCH_FAILED_MSG} from '../../constants';
 import './styles.less';
 
-
 const ImpulseAverageCountPanel = ({data, isLoading}) => {
-    const setError = () => (!isLoading && !data ? 'NO DATA AVAILABLE' : false);
+    const setError = () => (!isLoading && !data ? FETCH_FAILED_MSG : false);
     return (
         <LoadingContainer isLoading={isLoading} error={setError()}>
-            <div className="widget-card percentage-change-boundary percentage-change-container" >{data.selectedLobs} BOOKING TRENDS
-                <div className="percentage-change-container-text">
-                    <p>Change Percentage: Monthly <span className="weekly-monthly-data-color">{data.weekly}%</span>; Weekly <span className="weekly-monthly-data-color">{data.monthly}%</span></p>
+            <div className="impulse-container-percentage-change">
+                <div className="widget-card percentage-change-boundary percentage-change-container" >{data.selectedLobs} BOOKING TRENDS
+                    <div className="percentage-change-container-text">
+                        <p>Change Percentage: Monthly <span className="weekly-monthly-data-color">{data.weekly}%</span>; Weekly <span className="weekly-monthly-data-color">{data.monthly}%</span></p>
+                    </div>
                 </div>
             </div>
         </LoadingContainer>
