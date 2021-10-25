ac97c21dbac1243d6391e3bafce47bcf6e416056
Archana Ahirwar <aahirwar@expediagroup.com>
2021-10-25 12:03:51 +0530

    color change

--- src/universal/pages/Impulse/tabs/BookingTrends/sections/BookingChart/BookingChart.js
+++ src/universal/pages/Impulse/tabs/BookingTrends/sections/BookingChart/BookingChart.js
@@ -250,7 +250,7 @@ const BookingChart = ({
                     <CartesianGrid strokeDasharray="3 3"/>
                     <Tooltip content={<CustomTooltip/>}/>
                     {IMPULSE_CHART_TYPE.map(renderChart)}
-                    { data.length && data[0].hasOwnProperty(YOY_COUNT) ? <Line type="monotone" dataKey={YOY_COUNT} stroke="#3CB371" yAxisId={1} strokeWidth={1.5} dot={false} animationDuration={300} hide = {hiddenKeys.includes(YOY_COUNT)}/> : ''}
+                    { data.length && data[0].hasOwnProperty(YOY_COUNT) ? <Line type="monotone" dataKey={YOY_COUNT} stroke="#00008b" yAxisId={1} strokeWidth={0.5} dot={false} animationDuration={300} hide = {hiddenKeys.includes(YOY_COUNT)}/> : ''}
                     { data.length && data[0].hasOwnProperty(PREDICTION_COUNT) ? <Line type="monotone" dataKey={PREDICTION_COUNT} stroke="#c9405b" yAxisId={1} strokeWidth={1.5} dot={false} animationDuration={300} hide = {hiddenKeys.includes(PREDICTION_COUNT)}/> : ''}
                     {
                         anomalies && anomalies.map((anomaly) => (
