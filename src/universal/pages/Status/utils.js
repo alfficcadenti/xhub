e850080b342bf9223ec55ed089c799a541462df7
Mio Chung <t-miochung@expediagroup.com>
2021-11-25 14:16:12 +0000

    add logic to include timeout and add tests

--- src/universal/pages/Status/utils.js
+++ src/universal/pages/Status/utils.js
@@ -3,3 +3,9 @@ export const compareArraysElements = (array1, array2) => (
     array1.length === array2.length &&
     array1.every((result) => array2.includes(result)
     ));
+
+export const timeout = (time) => {
+    let controller = new AbortController();
+    setTimeout(() => controller.abort(), time * 1000);
+    return controller;
+};
