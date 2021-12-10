fe285dcccfb3eb2c02c85dd069bf0896b188f77e
Bianca Iordache <t-biordache@expediagroup.com>
2021-12-10 13:48:42 +0000

    refactor tests

--- src/universal/pages/TicketTrends/IncidentTrendsDashboard/tabs/Incident/tests/Incident.test.js
+++ src/universal/pages/TicketTrends/IncidentTrendsDashboard/tabs/Incident/tests/Incident.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {shallow} from 'enzyme/build';
 import {Incident} from '../..';
 import mockData from './filteredData.test.json';
-import {render, act, fireEvent, getByLabelText, getByText} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import {render, act, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 
 
 describe('Incident component testing', () => {
@@ -33,9 +34,32 @@ describe('Incident component testing', () => {
             wrapper = render(<Incident />);
             const input = wrapper.getByLabelText('Search incident by ID');
             fireEvent.change(input, {target: {value: 'INC123'}})
-            fireEvent.click(wrapper.getByText('Search'));
+            
         });
+        await act(async ()=> {
+            await fireEvent.click(wrapper.getByText('Search'));
+        })
 
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('returns error', async () => {
+        fetch.mockImplementation(async () => {
+            return Promise.resolve({
+                ok: false,
+                json: () => Promise.resolve([]) 
+            });
+        });
+        await act(async () => {
+            wrapper = render(<Incident />);
+            const input = wrapper.getByLabelText('Search incident by ID');
+            fireEvent.change(input, {target: {value: 'INC123'}})
+            
+        });
+        await act(async ()=> {
+            await fireEvent.click(wrapper.getByText('Search'));
+        })
+        expect(wrapper.getByText('Failed to retrieve data. Try refreshing the page. If the problem persists, please message #opxhub-support or fill out our Feedback form.')).toBeInTheDocument();
         expect(wrapper).toMatchSnapshot();
     });
 
