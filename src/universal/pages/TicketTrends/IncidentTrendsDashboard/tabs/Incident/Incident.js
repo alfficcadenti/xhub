93d2646f50e34d32bfa6cf41f36dd62f711422d7
Bianca Iordache <t-biordache@expediagroup.com>
2022-02-02 12:49:14 +0000

    refactor logic to reformat data table

--- src/universal/pages/TicketTrends/IncidentTrendsDashboard/tabs/Incident/Incident.js
+++ src/universal/pages/TicketTrends/IncidentTrendsDashboard/tabs/Incident/Incident.js
@@ -1,12 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import DataTable from '../../../../../components/DataTable';
-import {getIncidentsDataById} from '../../../incidentsHelper';
 import FormInput from '@homeaway/react-form-components/lib/transpiled/FormInput';
 import {checkResponse, consolidateTicketsById} from '../../../../utils';
 import {FETCH_FAILED_MSG} from '../../../../../constants';
-import {getTableColumnsForIncident} from '../../../incidentsHelper';
-import {COLUMNS_INFO} from '../../constants';
+import {formatObjectFromIncidentArray} from '../../../incidentsHelper';
 import LoadingContainer from '../../../../../components/LoadingContainer';
+import {INCIDENT_COLUMNS, INCIDENT_COLUMNS_LONG} from '../../../../Fci/constants';
+import './styles.less';
 
 
 const Incident = () => {
@@ -22,7 +21,7 @@ const Incident = () => {
             try {
                 const response = await fetch(`/v1/incidents/${search}`);
                 const resJson = await checkResponse(response);
-                setData(getIncidentsDataById(consolidateTicketsById(resJson)) || []);
+                setData(formatObjectFromIncidentArray(consolidateTicketsById(resJson)?.[0]));
                 setError('');
             } catch (e) {
                 setError(FETCH_FAILED_MSG);
@@ -31,33 +30,55 @@ const Incident = () => {
         fetchData().finally(() => setIsLoading(false));
     }, [search]);
 
+
+    const renderRow = (label, value) => {
+        return (
+            <div className="inc-row" key={label}>
+                <div className="inc-label">{label}</div>
+                <div className="inc-value">{value ? value : '-'}</div>
+            </div>
+        );
+    };
+
+    const renderLongRow = (label, value) => {
+        return (
+            <div className="inc-long-row" key={label}>
+                <div className="inc-long-label">{label}</div>
+                <div className="inc-long-value">{value ? value : '-'}</div>
+            </div>
+        );
+    };
+
     const renderTable = () => {
         return (
             <LoadingContainer isLoading={isLoading} error={error} >
-                <DataTable
-                    data={data}
-                    columns={getTableColumnsForIncident()}
-                    columnsInfo={COLUMNS_INFO}
-                    expandableColumns={['Details']}
-                    paginated={false}
-                />
+                <div className="inc-details-container">
+                    <div id="shortDetails">
+                        {INCIDENT_COLUMNS
+                            .map((column) => renderRow(column, data?.[column]))}
+                    </div>
+                    <div id="longDetails">
+                        {INCIDENT_COLUMNS_LONG
+                            .map((column) => renderLongRow(column, data?.[column]))}
+                    </div>
+                </div>
             </LoadingContainer>
         );
     };
 
     return (
         <div >
-            <div className="form-container fci-container">
+            <div className="inc-form-container">
                 <FormInput
-                    id="search-input"
+                    id="searchInput"
                     name="searchInput"
                     label="Search incident by ID"
-                    className="fci-search-input"
+                    className="inc-search-input"
                     value={pendingSearch}
                     onChange={(e) => setPendingSearch(e.target.value)}
                 />
                 <button
-                    className="btn btn-primary apply-btn"
+                    className="btn btn-primary search-btn"
                     type="button"
                     onClick = {(() => setSearch(pendingSearch))}
                 >
