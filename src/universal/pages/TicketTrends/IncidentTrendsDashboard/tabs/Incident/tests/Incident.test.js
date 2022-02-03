b4f23af6c5f9cd650e89ed9a5ab13cd17f87a19a
Bianca Iordache <t-biordache@expediagroup.com>
2022-02-03 11:29:18 +0000

    add more test cases to Incident component

--- src/universal/pages/TicketTrends/IncidentTrendsDashboard/tabs/Incident/tests/Incident.test.js
+++ src/universal/pages/TicketTrends/IncidentTrendsDashboard/tabs/Incident/tests/Incident.test.js
@@ -22,7 +22,7 @@ describe('Incident component testing', () => {
         expect(wrapper).toHaveLength(1);
     });
 
-    it('returns mocked data', async () => {
+    it('renders a table and matches snapshot when data is passed correctly', async () => {
         fetch.mockImplementation(async () => {
             return Promise.resolve({
                 ok: true,
@@ -41,7 +41,26 @@ describe('Incident component testing', () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    it('returns error', async () => {
+    it('renders NO RESULTS FOUND and matches the snapshot when data is empty', async () => {
+        fetch.mockImplementation(() => {
+            return Promise.resolve({
+                ok: true,
+                json: () => Promise.resolve([])
+            });
+        });
+        await act(async () => {
+            wrapper = render(<Incident />);
+            const input = wrapper.getByLabelText('Search incident by ID');
+            fireEvent.change(input, {target: {value: 'INC123'}});
+        });
+        await act(async () => {
+            await fireEvent.click(wrapper.getByText('Search'));
+        });
+        expect(wrapper.getByText(/NO RESULTS FOUND/)).toBeInTheDocument();
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('renders Error message when API returns error', async () => {
         fetch.mockImplementation(async () => {
             return Promise.resolve({
                 ok: false,
@@ -57,6 +76,7 @@ describe('Incident component testing', () => {
             await fireEvent.click(wrapper.getByText('Search'));
         });
         expect(wrapper.getByText('Failed to retrieve data. Try refreshing the page. If the problem persists, please message #opxhub-support or fill out our Feedback form.')).toBeInTheDocument();
+        expect(wrapper.getByRole('alert')).toBeInTheDocument();
         expect(wrapper).toMatchSnapshot();
     });
 });
