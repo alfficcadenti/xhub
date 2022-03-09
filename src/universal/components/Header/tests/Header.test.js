b396f6bdca4ecb9bb12e9469319180912819e834
Bianca Iordache <t-biordache@expediagroup.com>
2022-03-09 09:49:44 +0000

    add tests and modify existing ones to remove chai library

--- src/universal/components/Header/tests/Header.test.js
+++ src/universal/components/Header/tests/Header.test.js
@@ -1,31 +1,51 @@
 import React from 'react';
-import {mount} from 'enzyme';
 import Header from '../index';
-import {expect} from 'chai';
 import sinon from 'sinon';
-import {BRANDS, EXPEDIA_BRAND, EG_BRAND, VRBO_BRAND} from '../../../constants';
+import {BRANDS, EXPEDIA_BRAND, EG_BRAND,VRBO_BRAND} from '../../../constants';
 import {BrowserRouter} from 'react-router-dom';
-
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 const validBrands = BRANDS.map((brand) => brand.label);
 
 describe('<Header />', () => {
-    it('renders Platform Health and Availability dropdown menus', () => {
-        const wrapper = mount(<BrowserRouter><Header selectedBrands={[EXPEDIA_BRAND]} onBrandChange={sinon.spy} brands={validBrands} /></BrowserRouter>);
-        expect(wrapper.find('[id="Platform Health & Resiliency-dropdown--container"]').children()).to.have.length(8);
-        expect(wrapper.find('[id="Availability & Trends-dropdown--container"]').children()).to.have.length(6);
+    it('renders Platform Health and Availability dropdown menus if brand is Expedia Brand', () => {
+        const wrapper = render(<BrowserRouter><Header selectedBrands={[EXPEDIA_BRAND]} onBrandChange={sinon.spy} brands={validBrands} /></BrowserRouter>);
+        expect((wrapper.getAllByRole('dialog')[0]).children).toHaveLength(6); // Availability & Trends
+        expect((wrapper.getAllByRole('dialog')[1]).children).toHaveLength(8); // Platform Health & Resiliency
     });
 
     it('renders only Platform Health dropdown menu if brand is EG GROUP', () => {
-        const wrapper = mount(<BrowserRouter><Header selectedBrands={[EG_BRAND]} onBrandChange={sinon.spy} brands={validBrands} /></BrowserRouter>);
-        expect(wrapper.find('[id="Platform Health & Resiliency-dropdown--container"]').children()).to.have.length(8);
-        expect(wrapper.find('[id="Availability & Trends-dropdown--container"]').children()).to.have.length(3);
+        const wrapper = render(<BrowserRouter><Header selectedBrands={[EG_BRAND]} onBrandChange={sinon.spy} brands={validBrands} /></BrowserRouter>);
+        expect((wrapper.getAllByRole('dialog')[0]).children).toHaveLength(3); // Availability & Trends
+        expect((wrapper.getAllByRole('dialog')[1]).children).toHaveLength(8); // Platform Health & Resiliency
     });
 
     it('renders Platform Health, Availability and CE Analysis dropdown menus if brand is VRBO', () => {
-        const wrapper = mount(<BrowserRouter><Header selectedBrands={[VRBO_BRAND]} onBrandChange={sinon.spy} brands={validBrands} /></BrowserRouter>);
-        expect(wrapper.find('[id="Platform Health & Resiliency-dropdown--container"]').children()).to.have.length(9);
-        expect(wrapper.find('[id="Availability & Trends-dropdown--container"]').children()).to.have.length(12);
-        expect(wrapper.find('[id="Customer Experience Analysis-dropdown--container"]').children()).to.have.length(2);
+        const wrapper = render(<BrowserRouter><Header selectedBrands={[VRBO_BRAND]} onBrandChange={sinon.spy} brands={validBrands} /></BrowserRouter>);
+        expect((wrapper.getAllByRole('dialog')[0]).children).toHaveLength(12); // Availavbility & Trends
+        expect((wrapper.getAllByRole('dialog')[1]).children).toHaveLength(2); // CE Analysis
+        expect((wrapper.getAllByRole('dialog')[3]).children).toHaveLength(9); // Platform Health & Resiliency
     });
+
+    it('search box has no content before click event', () => {
+        const onChange = jest.fn();
+        const wrapper = render(<BrowserRouter><Header selectedBrands={[EXPEDIA_BRAND]} onBrandChange={sinon.spy} brands={validBrands} onChange={onChange} /></BrowserRouter>);
+        expect(wrapper).toMatchSnapshot();
+        let input = screen.queryByRole(/textbox/);
+        expect(input).not.toHaveTextContent;
+        fireEvent.click(input);
+        expect(input).toHaveTextContent;
+    });
+
+    it('text input value changes after making selection', () => {
+        const onChange = jest.fn();
+        const wrapper = render(<BrowserRouter><Header selectedBrands={[EXPEDIA_BRAND]} onBrandChange={sinon.spy} brands={validBrands} onChange={onChange} /></BrowserRouter>);
+        expect(wrapper).toMatchSnapshot();
+        let input = screen.queryAllByRole(/textbox/);
+        expect(input).toHaveLength(1);
+        expect(input).not.toHaveTextContent;
+        fireEvent.change(screen.queryAllByRole(/textbox/)[0], {target: {value: 'Home'}});
+        expect(input[0]).toHaveValue('Home');
+    })
 });
