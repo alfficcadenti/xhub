169d673a6e57735cac6b2bdc77117382c3a40793
Archana Ahirwar <aahirwar@expediagroup.com>
2022-04-12 12:15:10 +0530

    add device type column in more details

--- src/universal/pages/Impulse/tabs/BookingTrends/sections/AnomalyTable/constants.js
+++ src/universal/pages/Impulse/tabs/BookingTrends/sections/AnomalyTable/constants.js
@@ -2,6 +2,7 @@ export const ANOMALY_TABLE_COLUMN_HEADERS = {
     timestamp: 'Timestamp',
     lob: 'LOB',
     brand: 'Brand',
+    deviceType: 'Device Type',
     count: 'Count',
     predicted: 'Predicted',
     changePercentage: 'Percentage Change'
@@ -11,6 +12,7 @@ export const ANOMALY_TABLE_COLUMNS = [
     'timestamp',
     'lob',
     'brand',
+    'deviceType',
     'count',
     'predicted',
     'changePercentage'
